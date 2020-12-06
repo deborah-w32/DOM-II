b55a0e78cbf872af6eec7267b0993c6ce3394143
Deborah Warren <deborah.dw.32@gmail.com>
2020-12-06 17:59:01 -0500

    MVP and stretch assignment attempt

---
+++ stretch-assignment/index.js
@@ -0,0 +1,41 @@
+// Selectors
+const redTraveler = document.getElementsByClassName('block--red');
+const blueTraveler = document.getElementsByClassName('block--blue');
+const greenTraveler = document.getElementsByClassName('block--green');
+const pinkTraveler = document.getElementsByClassName('block--pink');
+const grayTraveler = document.getElementsByClassName('block--gray');
+const body = document.querySelector('body');
+const orderBlocks = document.getElementsByClassName('blocks');
+const blocks = document.querySelectorAll('.blocks .block');
+const block = document.getElementsByClassName('block')
+
+// eventListener
+
+// click
+
+blocks.forEach(block => block.addEventListener('click', () => {
+    blocks.forEach(block => block.style.order = (block.style.order + 1));
+    block.style.order = 0; 
+}));
+
+// mousedown
+redTraveler[0].addEventListener('mousedown', () => {
+    redTraveler[0].style.transform = 'translateX(150vw)';
+    redTraveler[0].style.transition = '5s';
+});
+blueTraveler[0].addEventListener('mousedown', () => {
+    blueTraveler[0].style.transform = 'translateX(150vw)';
+    blueTraveler[0].style.transition = '5s';
+});
+greenTraveler[0].addEventListener('mousedown', () => {
+    greenTraveler[0].style.transform = 'translateX(150vw)';
+    greenTraveler[0].style.transition = '5s';
+});
+pinkTraveler[0].addEventListener('mousedown', () => {
+    pinkTraveler[0].style.transform = 'translateX(150vw)';
+    pinkTraveler[0].style.transition = '5s';
+});
+grayTraveler[0].addEventListener('mousedown', () => {
+    grayTraveler[0].style.transform = 'translateX(150vw)';
+    grayTraveler[0].style.transition = '5s';
+});
